73bd888110a33d678210e1374f7488cb7b8ed971
KKoaLLa04 <ndkdzvl@gmail.com>
2023-11-01 20:25:58 +0700

    second commit

---
+++ push_exams_db.sql
@@ -0,0 +1,34 @@
+ALTER TABLE `spring_blocks` DROP FOREIGN KEY `spring_blocks_fk0`;
+
+ALTER TABLE `subjects` DROP FOREIGN KEY `subjects_fk0`;
+
+ALTER TABLE `examinations` DROP FOREIGN KEY `examinations_fk0`;
+
+ALTER TABLE `examinations` DROP FOREIGN KEY `examinations_fk1`;
+
+ALTER TABLE `examinations` DROP FOREIGN KEY `examinations_fk2`;
+
+ALTER TABLE `subject_medias` DROP FOREIGN KEY `subject_medias_fk0`;
+
+ALTER TABLE `subject_medias` DROP FOREIGN KEY `subject_medias_fk1`;
+
+ALTER TABLE `subject_medias` DROP FOREIGN KEY `subject_medias_fk2`;
+
+ALTER TABLE `examination_medias` DROP FOREIGN KEY `examination_medias_fk0`;
+
+ALTER TABLE `examination_medias` DROP FOREIGN KEY `examination_medias_fk1`;
+
+ALTER TABLE `examination_medias` DROP FOREIGN KEY `examination_medias_fk2`;
+
+ALTER TABLE `examination_teachers` DROP FOREIGN KEY `examination_teachers_fk0`;
+
+ALTER TABLE `examination_teachers` DROP FOREIGN KEY `examination_teachers_fk1`;
+
+ALTER TABLE `examination_teachers` DROP FOREIGN KEY `examination_teachers_fk2`;
+
+ALTER TABLE `examination_histories` DROP FOREIGN KEY `examination_histories_fk0`;
+
+ALTER TABLE `examination_histories` DROP FOREIGN KEY `examination_histories_fk1`;
+
+ALTER TABLE `examination_histories` DROP FOREIGN KEY `examination_histories_fk2`;
+
